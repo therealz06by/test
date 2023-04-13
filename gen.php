4df9e4bda75246f75c738534145d9ff8db5ebbf6
drx1r <penohiopen@gmail.com>
2023-04-12 22:42:12 -0400

    1111

---
+++ gen.php
@@ -0,0 +1,315 @@
+<link rel="icon" type="image/x.icon" href="icon.ico">
+
+<html lang="en" dir="ltr">
+<link rel="stylesheet" href="fonts.css">
+<link rel="stylesheet" href="fonts1.css">
+<title>Generator |XYZ 24/7 Account generator</title>
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@600&display=swap" rel="stylesheet">
+
+    <meta charset="utf-8">
+
+<head>
+
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1144587263160047"
+     crossorigin="anonymous"></script>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+<div class="navbar">
+ <div class="discord">
+<img width="50px" height="50px" src="discord.png">
+</div>
+
+
+
+<div class="discord2">
+<a href="https://discord.gg/2TZg9aThP9"></i>Discord</a></div>
+  <a href="index.html">Home</a>
+ 
+  <div class="dropdown">
+    <button class="dropbtn">Accounts
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="gen.php">2010</a>
+      <a href="#">Soon....</a>
+   
+
+    </div>
+      </div>
+   
+      
+     <div class="dropdown">
+    <button class="dropbtn">Names
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="gen2.php">5 Char</a>
+      <a href="#">Soon....</a>
+      <a href="#">soon....</a>
+    </div>
+  </div
+</div>
+</head>
+
+
+<body>
+
+
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>  
+<br>
+<br>
+<br>
+<br>  
+
+
+</font>
+
+<style>
+
+ *
+     {
+     margin: 0;
+     padding: 0;
+   font-family: 'Roboto Slab', serif;
+     }
+
+body{
+
+   background-image: url("bg.png");
+}
+
+.discord{
+    float:right;
+    margin-right: 5%;
+}
+
+.discord2{
+    float:right;
+    margin-right: -1%;
+}
+
+.btn {
+  background-color: #ff9900;
+  border: none;
+  color: white;
+  padding: 12px 16px;
+  font-size: 16px;
+  cursor: pointer;
+}
+
+.button {
+  border: none;
+  color: Black;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  transition-duration: 0.4s;
+}
+
+.button:hover {
+  background-color: #000000; 
+  color: white;
+}
+
+.navbar {
+  overflow: hidden;
+  background-color: black;
+  font-family: Arial;
+  height: 6%;
+  
+}
+
+/* Links inside the navbar */
+.navbar a {
+  float: left;
+  font-size: 20px;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+/* The dropdown container */
+.dropdown {
+  float: left;
+  overflow: hidden;
+}
+
+/* Dropdown button */
+.dropdown .dropbtn {
+  font-size: 20px;
+  border: none;
+  outline: none;
+  color: #ff9900;
+  padding: 14px 16px;
+  background-color: inherit;
+  font-family: inherit; /* Important for vertical align on mobile phones */
+  margin: 0; /* Important for vertical align on mobile phones */
+}
+
+/* Add a red background color to navbar links on hover */
+.navbar a:hover, .dropdown:hover .dropbtn {
+  background-color: black;
+}
+
+/* Dropdown content (hidden by default) */
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #202020;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+/* Links inside the dropdown */
+.dropdown-content a {
+  float: none;
+  color: #ff9900;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+  text-align: left;
+}
+
+/* Add a grey background color to dropdown links on hover */
+.dropdown-content a:hover {
+  background-color: #ddd;
+}
+
+/* Show the dropdown menu on hover */
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+
+.adv2
+{
+    border: #868ac2;
+    background-color: #868ac2;
+    width: 1px;
+    height: 1px;
+    float: right;
+    margin-right: 28%;
+}
+
+.view2
+{
+        height: 100%;
+    weight: 100%;
+    background-color: black;
+}
+
+.rund {
+border-radius: 12px;
+-moz-border-radius: 12px;
+-webkit-border-radius: 12px
+}
+
+
+
+.stein
+{
+background: rgba(255, 153, 0, 0.36);
+border-radius: 16px;
+box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+backdrop-filter: blur(17.2px);
+-webkit-backdrop-filter: blur(17.2px);
+border: 1px solid rgba(255, 153, 0, 0.97);
+width: 20%;
+}
+
+.rechts {
+float: right;
+margin-right: 20%;
+}
+
+.mit{
+    margin-left: 32%
+}
+
+.inputClass {
+            pointer-events: none; 
+              border: none;
+           background-color: transparent;
+           font-size: 20px;
+           width: 100%
+           margin-left: 
+}
+        
+.input {
+  border: none;
+}
+
+@media screen and (max-width: 600px)
+{
+
+
+
+}
+</style>
+
+
+<div class="adv2">
+<a href="leitung2.php"><img class="rund" weight="240px" height="520px" src="Banner.png"></a>
+</div>
+
+<center>
+<div class="stein"> 
+<font color="white">
+<h1>Roblox 2010 Account </h1>
+</font>
+<img width="100%" src="bild1.png">
+<font color="black" face="Arial">
+<h2><input class="inputClass" type="text" value="kklllkokkllllklklkok:l0l0l0l
+" id="myInput"> </h2>
+</font>
+
+<div class="rechts"><button onclick="myFunction()" class="btn"><i class="fa fa-clipboard"></i></button> </div>    <div class="mit">  <button class="button" onclick="window.location.href='gen.php';">Generate</button> </div> 
+
+</center>
+
+<br>
+
+<center>
+<font color="white" face="Arial">
+<p><h3><u>*Maybe not every Account work because they are taken retry a few times</h3></u></p>
+<br>
+<h2>Advertising:</h2>
+<a href="leitung.php"><img src="test.png"></a>
+</center>
+</div>
+</div>
+
+<script>
+function myFunction() {
+  var copyText = document.getElementById("myInput");
+
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+  navigator.clipboard.writeText(copyText.value);
+
+}
+
+
+
+</script>
+
+
+
+
+
+</body>
+</html>
